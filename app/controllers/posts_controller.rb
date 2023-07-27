23a23a2cc3352e12bb8293147796838c51088851
Sergio Saavedra <sergio.saavedra@ug.uchile.cl>
2023-07-26 20:25:09 -0400

    update post_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,20 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(posts_params)
+    if @post.save
+      redirect_to posts_index_path
+    else
+      render :new
+    end
   end
 
   def new
+    # Captura los datos enviados en el formulario
+    @post = Post.new
+  end
+
+  def posts_params
+    params.require(:post).permit(:title, :content)
   end
 end
