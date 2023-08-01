78d3b42c50a88ee4ce1115fd602cdd26474120dd
Sergio Saavedra <sergio.saavedra@ug.uchile.cl>
2023-08-01 05:16:36 -0400

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  # Rutas para el controlador journalists
   get 'journalists/index'
   get 'journalists/create'
   get 'journalists/new'
-  # Rutas get
+
+  # Rutas para el controlador posts
   get 'posts/index' => 'posts#index'
   get 'posts/new' => 'posts#new'
   get 'posts/create' => 'posts#create'
